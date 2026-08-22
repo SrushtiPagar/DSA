3a5f21530ffd7832598f48b7260cfe7badfb768f
SrushtiPagar <srushtipagar814@gmail.com>
2026-08-22 13:15:01 +0530

    Add NestedLoop.cpp

---
+++ NestedLoop.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+//	for(int i = 1;i<=5 ; i++ )
+//	{
+//		cout<<"* * * * *"<<endl;;
+//	}
+	
+	// Print n no. of stars;
+//	int n;
+//	cin>>n;
+//	for(int i=1;i<=n ; i++)
+//	{
+//		cout<<"* ";
+//	}
+
+	//print number of stars in number of lines;
+//	int n;
+//	cin>>n;
+//	int m;
+//	cin>>m;
+//	for(int i = 1;i<=n ; i++ )
+//	{
+//		for(int j=1;j<=m ; j++)
+//		{
+//			cout<<"* ";
+//		}
+//		cout<<endl;;
+//	}
+
+	//Patterns 
+	
+	
+	
+}
