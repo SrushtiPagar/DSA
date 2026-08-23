44cf74f0ab50c75045859be13784bf09df59a1af
SrushtiPagar <srushtipagar814@gmail.com>
2026-08-23 13:12:01 +0530

    Add loops.cpp

---
+++ loops.cpp
@@ -0,0 +1,35 @@
+//loops
+#include <iostream>
+using namespace std;
+
+int main()
+{
+//	int count = 1;
+	//print 1 to 5 numbers
+	//while
+//	while(count<=6)
+//	{
+//		cout<<count<<endl;
+//		count++;
+//	}
+	//print upto n;
+	int n;
+	cin>>n;
+//	while(count<=n)
+//	{
+//		cout<<count<<endl;
+//		count++;
+//	}
+
+	//for
+	//print 1 to 5
+//	for(int i=1 ; i<=5 ; i++)
+//	{
+//		cout<<i<<endl;
+//	}
+	//upto n;
+	for(int i=1 ; i<=n ; i=i+2)
+	{
+		cout<<i<<endl;
+	}
+}
