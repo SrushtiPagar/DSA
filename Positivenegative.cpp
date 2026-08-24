5b775339e5109090411218b642683f971ba640a6
SrushtiPagar <srushtipagar814@gmail.com>
2026-08-24 13:16:01 +0530

    Add Positivenegative.cpp

---
+++ Positivenegative.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n= -45;
+	
+//	if(n>=0)
+//	{
+//		cout<<"Positive";
+//	}
+//	else
+//	{
+//		cout<<"Negative";
+//	}
+
+	//second approach
+	//ternary statement 
+	cout<<(n>=0 ? "Positive" : "Negative");
+}
